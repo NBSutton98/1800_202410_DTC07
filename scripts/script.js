343a863c005e3b3c2464f22c62ec530b714fba0c
Caley R <crombout@my.bcit.ca>
2024-10-28 18:00:57 -0700

    Added base template & about-us.html

--- scripts/script.js
+++ scripts/script.js
@@ -2,28 +2,6 @@
 // Your own functions here
 //---------------------------------
 
-// Function to show the popup
-function showPopup() {
-  const popup = document.getElementById('popup');
-  const overlay = document.getElementById('overlay');
-  popup.classList.add('show');
-  overlay.classList.add('show');
-}
-
-// Function to close the popup
-function closePopup() {
-  const popup = document.getElementById('popup');
-  const overlay = document.getElementById('overlay');
-  popup.classList.remove('show');
-  overlay.classList.remove('show');
-}
-
-// Automatically show the popup after 3 seconds (3000 milliseconds)
-setTimeout(showPopup, 2500);
-
-// Event listener for closing the popup
-document.getElementById('closePopup').onclick = closePopup;
-document.getElementById('overlay').onclick = closePopup;
 
 //------------------------------------------------
 // Call this function when the "logout" button is clicked
