9f0f9175be21efb605dfd9b5bb7f746cb1527926
Timothy kim <timothyakim21@gmail.com>
2024-11-05 23:05:30 -0800

    add to do list task display

---
+++ scripts/to-do-list-task.js
@@ -0,0 +1,21 @@
+function displayTaskInfo() {
+    let params = new URL(window.location.href); //get URL of search bar
+    let ID = params.searchParams.get("docID"); //get value for key "id"
+
+    db.collection("tasks")
+        .doc(ID)
+        .get()
+        .then(doc => {
+            taskTitle = doc.data().task;
+            dueDate = doc.data().dueDate;
+            description = doc.data().description;
+
+            // only populate title, and image
+            document.getElementById("task-display").innerHTML = taskTitle;
+            document.getElementById("duedate-display").innerHTML = dueDate;
+            document.getElementById("description-display").innerHTML = description;
+
+        });
+}
+
+displayTaskInfo();
