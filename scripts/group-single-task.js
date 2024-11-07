2e79d60ef1336f9fb7e3fd3b0c4ca1cdcafaf22e
Timothy kim <timothyakim21@gmail.com>
2024-11-07 01:09:29 -0800

    add function to update database

--- scripts/group-single-task.js
+++ scripts/group-single-task.js
@@ -11,11 +11,40 @@ function displayTaskInfo() {
             description = doc.data().description;
 
             // only populate title, and image
-            document.getElementById("task-display").innerHTML = taskTitle;
-            document.getElementById("duedate-display").innerHTML = dueDate;
-            document.getElementById("description-display").innerHTML = description;
+            document.getElementById("task-display").value = taskTitle;
+            document.getElementById("duedate-display").value = dueDate;
+            document.getElementById("description-display").value = description;
 
         });
 }
 
 displayTaskInfo();
+
+function editTaskInfo() {
+    //Enable the form fields
+    document.getElementById('taskInfoFields').disabled = false;
+}
+
+function saveTaskInfo() {
+    //enter code here
+
+    let params = new URL(window.location.href); //get URL of search bar
+    let ID = params.searchParams.get("docID");
+
+    //a) get user entered values
+    taskName = document.getElementById('task-display').value;       //get the value of the field with id="nameInput"
+    duedate = document.getElementById('duedate-display').value;     //get the value of the field with id="schoolInput"
+    descriptionInfo = document.getElementById('description-display').value;       //get the value of the field with id="cityInput"
+
+    //b) update user's document in Firestore
+    db.collection("tasks")
+    .doc(ID)
+    .update({
+        dueDate: duedate,
+        task: taskName,
+        description: descriptionInfo
+    })    
+
+
+    document.getElementById('personalInfoFields').disabled = true;
+}
