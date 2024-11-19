6a28256cb6ed1d0c6316c7ff8844aa237b533d6d
Timothy Kim <timothyakim21@gmail.com>
2024-11-19 13:53:33 -0800

    delete useless code

--- scripts/user-profile.js
+++ scripts/user-profile.js
@@ -1,42 +1,4 @@
 var currentUser;               //points to the document of the user who is logged in
-document.getElementById('cancel-btn').style.display = 'none';
-
-console.log(document.getElementById('dropdown').value)
-
-function editInfo() {
-    document.getElementById('userInfoFields').disabled = false;
-    document.getElementById('edit-btn').style.display = 'none';
-    document.getElementById('cancel-btn').style.display = 'block';
-}
-
-function cancelEdit() {
-    document.getElementById('userInfoFields').disabled = true;
-    document.getElementById('edit-btn').style.display = 'block';
-    document.getElementById('cancel-btn').style.display = 'none';
-    populateUserInfo();
-}
-
-
-function saveInfo() {
-    console.log(document.getElementById('dropdown').value)
-
-    userName = document.getElementById('Buddy-name').value;
-    userBuddy = document.getElementById('dropdown').value;
-
-
-    currentUser.update({
-        name: userName,
-        buddy: userBuddy
-    })
-        .then(() => {
-            console.log("Document successfully updated!");
-        })
-    document.getElementById('userInfoFields').disabled = true;
-    document.getElementById('edit-btn').style.display = 'block';
-    document.getElementById('cancel-btn').style.display = 'none';
-    populateUserInfo()
-}
-
 
 function populateUserInfo() {
     firebase.auth().onAuthStateChanged(user => {
@@ -58,10 +20,8 @@ function populateUserInfo() {
                     if (userBuddy != null) {
                         document.getElementById("dropdown").value = userBuddy;
                     }
+                    document.getElementById('buddy').src = `./assets/animals/${userBuddy}.svg`
                 })
-            document.getElementById('buddy').src = `"./assets/animals/"${userBuddy}'.svg'`
-
-
         } else {
             // No user is signed in.
             console.log("No user is signed in");
